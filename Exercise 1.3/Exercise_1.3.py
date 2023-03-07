e547f41f3e83dda1df8725ad4c2b357144942f82
Brian Smawley <briansmawley@gmail.com>
2023-03-07 17:30:13 -0500

    Completion of 1.3 with learning journal

--- Exercise 1.3/Exercise_1.3.py
+++ Exercise 1.3/Exercise_1.3.py
@@ -1,17 +1,18 @@
 recipes_list = []
 ingredients_list = []
 
+
 def take_recipe():
     recipe_name = input("Enter the name of the recipe: ")
-    recipe_ingredients = input("Enter the ingredients of the recipe: ")
-    recipes_list.append(recipe_name)
-    ingredients_list.append(recipe_ingredients)
+    recipe_ingredients = input("Enter the ingredients of the recipe: ").split(", ")
     cooking_time = int(input("Enter the cooking time of the recipe: "))
-    recipe={"name":recipe_name,"ingredients":recipe_ingredients,"cooking_time":cooking_time}
+    recipe = {"name": recipe_name, "ingredients": recipe_ingredients,
+              "cooking_time": cooking_time}
     return recipe
 
+
 n = int(input("Enter the number of recipes: "))
-# recipe = {}
+
 for number in range(n):
     recipe = take_recipe()
     for ingredient in recipe['ingredients']:
@@ -22,18 +23,20 @@ for number in range(n):
     recipes_list.append(recipe)
 
 for recipe in recipes_list:
-    if recipe['cooking_time'] < 10 and len(recipe['ingredient']) < 4:
+    if recipe['cooking_time'] < 10 and len(recipe['ingredients']) < 4:
         recipe['difficulty'] = "easy"
-    elif recipe['cooking_time'] < 10 and len(recipe['ingredient']) <= 4:
-       recipe['difficulty'] = "medium"
-    elif recipe['cooking_time'] >= 10 and len(recipe['ingredient']) < 4:
-       recipe['difficulty'] = "intermediate"
-    elif recipe['cooking_time'] >= 10 and len(recipe['ingredient']) >= 4:
-       recipe['difficulty'] = "hard"
+    elif recipe['cooking_time'] < 10 and len(recipe['ingredients']) <= 4:
+        recipe['difficulty'] = "medium"
+    elif recipe['cooking_time'] >= 10 and len(recipe['ingredients']) < 4:
+        recipe['difficulty'] = "intermediate"
+    elif recipe['cooking_time'] >= 10 and len(recipe['ingredients']) >= 4:
+        recipe['difficulty'] = "hard"
 
 for recipe in recipes_list:
     print("Recipe name: ", recipe["name"])
-    print("Ingredients: ", recipe["ingredients"])
+    print("Ingredients: ")
+    for ingredient in recipe["ingredients"]:
+        print(ingredient)
     print("Cooking time: ", recipe["cooking_time"])
     print("Difficulty: ", recipe["difficulty"])
 
